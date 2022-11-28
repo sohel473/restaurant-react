ae7d8f1d3ee9fec0b3846e420140d8065164d5c6
Abdullah Al Sohel <absohel47@gmail.com>
2022-11-28 17:48:36 +0600

    Designing Menu Item

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,6 +1,30 @@
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
 const MenuItem = (props) => {
-  console.log(props);
-  return <div></div>;
+  // console.log(props);
+  return (
+    <div>
+      <Card style={{ margin: "10px", width: "30rem" }}>
+        <CardBody>
+          <CardImg
+            width="100%"
+            alt={props.dish.name}
+            src={props.dish.image}
+            style={{ opacity: "0.5" }}
+          />
+          <CardImgOverlay>
+            <CardTitle tag="h5">{props.dish.name}</CardTitle>
+          </CardImgOverlay>
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default MenuItem;
