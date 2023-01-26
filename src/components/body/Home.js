c6f0ab1daa56470921f82f3e4b15b20a68d116b1
Sohel <absohel47@gmail.com>
2023-01-26 17:26:41 +0000

    map Dispatch To Props

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => {
+  console.log("mapStateToProps: ", state);
+  return {
+    dishes: state.dishes,
+    sample: state.sample,
+  };
+};
 
 class Home extends Component {
+  componentDidMount() {
+    console.log("Home State: ", this.state);
+    console.log("Home Props: ", this.props);
+  }
   render() {
-    document.title = "Home";
-
+    document.title = "Bohubrihi Restaurant";
     return <div></div>;
   }
 }
 
-export default Home;
+export default connect(mapStateToProps)(Home);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,13 +4,13 @@ import COMMENTS from "../data/comments";
 const initialState = {
   dishes: DISHES,
   comments: COMMENTS,
+  sample: "Hello world",
 };
 
 export const Reducer = (state = initialState, action) => {
   // console.log("From Reducer ", action);
   if (action.type === "ADD_COMMENT") {
     let comment = action.payload;
-    console.log(comment);
     comment.id = state.comments.length;
     comment.date = new Date().toDateString();
     console.log(comment);
